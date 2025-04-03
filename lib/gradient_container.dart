8e9f42a8fdfdfa6e1c1558a4ed75e9c04e2520c5
Wilson Owino <37038018+Wilson-Ochieng@users.noreply.github.com>
2025-04-03 10:00:49 +0300

    Made my code reusable

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,8 +1,8 @@
 import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 //Created variables instead of hardcoding
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const  startAlignment = Alignment.topLeft;
+const  endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   //Constuctor Function For  GradientContainer
@@ -11,8 +11,8 @@ class GradientContainer extends StatelessWidget {
   @override
   build(context) {
     return Container(
-      decoration:  BoxDecoration(
-        gradient: LinearGradient(colors: [
+      decoration: const  BoxDecoration(
+        gradient:const LinearGradient(colors: [
           Color.fromARGB(205, 83, 101, 130),
           Color.fromARGB(255, 46, 165, 13)
         ], begin: startAlignment, end: endAlignment),
