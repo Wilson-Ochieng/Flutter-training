d0a10edbcc7602c39c7f86f19f14fd43ac4b36c3
Wilson Owino <37038018+Wilson-Ochieng@users.noreply.github.com>
2025-04-05 13:10:40 +0300

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,10 @@
-//This is the entry point of our application
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( const MaterialApp(
-    home: Scaffold(
-      backgroundColor: Color.fromRGBO(255, 9, 255,20),
-      body: Center(
-        child:  Text(style: 
-        TextStyle(
-          fontSize: 34.8,
-          fontWeight: FontWeight.bold,
-          color: Color.fromARGB(202, 222, 52, 0),
-        ),'Hello, World!'),
-      ),
+  runApp(
+    const MaterialApp(
+      home: 
+      Center(child: Text('Hello, World!')),
     ),
-  ));
+  );
 }
